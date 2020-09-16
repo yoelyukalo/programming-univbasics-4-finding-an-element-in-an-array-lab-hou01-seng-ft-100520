bffdd8310ef57e87465e55798e9c92f61f3bb0b1
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-16 17:59:36 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,11 @@
 def find_element_index(array, value_to_find)
   count = 0 
+  found_value_index = nil 
   while count < array.length do
-    array[count] == value_to_find
+    if array[count] == value_to_find
+      found_value_index = count
+    end
     count += 1 
   end
+  found_value_index
 end
